7d0c402cf4af6e43812b783d957a5ede226240fc
Rahul Maheshwari <rahul.191ch039@nitk.edu.in>
2021-07-27 08:45:58 +0530

    Contact us changes

--- src/Contact.js
+++ src/Contact.js
@@ -38,25 +38,32 @@ emailjs.sendForm('service_tgz6l3n','template_qzmh8vl',e.target,'user_8qS1kBpLps2
                     <div className="col-md-6 col-10 mx-auto">
                         <form onSubmit={formSubmit}>
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Name</label>
+                                <label for="exampleInputEmail1" style={{fontSize:'20px'}}>Name</label>
                                 <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="Fullname" value={data.Fullname} onChange={InputEvent} placeholder="Enter Your Name" />
 
                             </div>
+                            
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Mobile</label>
+                                <label for="exampleInputEmail1" style={{fontSize:'20px'}}>Mobile</label>
                                 <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="Phone" value={data.Phone} onChange={InputEvent} placeholder="Enter Your Mobile Number" />
 
                             </div>
+                            <br></br>
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Email</label>
+                                <label for="exampleInputEmail1" style={{fontSize:'20px'}}>Email</label>
                                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="Email" value={data.Email} onChange={InputEvent} placeholder="Enter Your Email" />
                                 
                             </div>
+                            <br></br>
                             <div class="form-group">
-                                <label for="exampleFormControlTextarea1">Message/Suggestions</label>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="4" name="Msg" value={data.Msg} onChange={InputEvent} placeholder="Enter Your Message"></textarea>
+                                <label for="exampleFormControlTextarea1" style={{fontSize:'20px'}}>Message/Suggestions</label>
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="8" name="Msg" value={data.Msg} onChange={InputEvent} placeholder="Enter Your Message/Suggestions"></textarea>
                             </div>
+                            <br></br>
+                            {/* <br></br> */}
+                            <br></br>
                             <button type="submit" class="btn btn-outline-primary">Submit</button>
+                            <br></br>
                         </form>
                     </div>
                 </div>
