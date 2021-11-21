57b8321f46a71aa5a06454770454fd4a795f6820
Rahul Maheshwari <rahul.191ch039@nitk.edu.in>
2021-11-21 12:30:00 +0530

    Chat App Added

--- src/Projects.js
+++ src/Projects.js
@@ -12,6 +12,7 @@ import pix from '../src/images/pixel.jpg'
 import biogas from '../src/images/biogas.jpg'
 import expense from '../src/images/Expense.jpg'
 import Book from '../src/images/Book.jpg'
+import Chat from '../src/images/Chat.jpg'
 const Projects = () => {
     return (
         <>
@@ -33,15 +34,15 @@ const Projects = () => {
                                                 • For hosting I have used Heroku Platform :)
 
 
-</p>
+                                            </p>
                                             <a href="https://github.com/Rahul27012000/Ecommerce-Website" className="btn-get-started" target="_blank">Can Check it here</a>&nbsp;
                                             <a href="https://ecommercerahulwebsite.herokuapp.com/" className="btn-get-started" target="_blank">Tap here to Visit</a>
                                         </div>
                                     </div>
                                 </div>
 
-                               
-                                
+
+
                                 <div className="col-md-4 col-10 mx-auto">
                                     <div className="card">
                                         <img className="card-img-top" src={port} alt="Card image cap" style={{ height: '195px' }} />
@@ -70,16 +71,30 @@ const Projects = () => {
                                         </div>
                                     </div>
                                 </div>
-               
+
                                 <div className="col-md-4 col-10 mx-auto">
                                     <div className="card">
-                                        <img className="card-img-top" src={expense} alt="Card image cap" style={{height:"31vh"}}/>
+                                        <img className="card-img-top" src={expense} alt="Card image cap" style={{ height: "31vh" }} />
                                         <div className="card-body">
                                             <h5 className="card-title" style={{ textAlign: "center" }}>Voice Powered Expense Tracker</h5><br></br>
                                             <p className="card-text">• I have created Voice Powered Expense Tracker where you don't have to type/enter anything as every single entry is Voice Powered.<br></br>
                                                 • Technologies used: React.JS ,ChartJS ,React-Speechly ,MaterialUI ,gh-pages for hosting.</p><br></br>
-                                                <a href="https://github.com/Rahul27012000/Voice_Powered_Budget_Tracker" className="btn-get-started" target="_blank">Can Check it here</a>&nbsp;
+                                            <a href="https://github.com/Rahul27012000/Voice_Powered_Budget_Tracker" className="btn-get-started" target="_blank">Can Check it here</a>&nbsp;
                                             <a href="https://rahul27012000.github.io/Voice_Powered_Budget_Tracker/" className="btn-get-started" target="_blank">Tap here to Visit</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-4 col-10 mx-auto">
+                                    <div className="card">
+                                        <img className="card-img-top" src={Chat} alt="Card image cap" style={{ height: "31vh" }} />
+                                        <div className="card-body">
+                                            <h5 className="card-title" style={{ textAlign: "center" }}>Chat Application</h5>
+                                            <p className="card-text">• I have created Chat Application where you can send Audio,Video,GIFs,Links, Documents,images etc, 
+                                                You can create groups,add members to it,make friends and have conversation with them!
+                                                <br></br>
+                                                • Technologies used: Stream-Chat,Crypto,ReactJS,ExpressJS,NodeJS,React-Stream-Chat,get stream etc!</p>
+                                            <a href="https://github.com/Rahul27012000/Chat_Application/" className="btn-get-started" target="_blank">Can Check it here</a>&nbsp;
+                                            <a href="https://chatappbyrahul.netlify.app/" className="btn-get-started" target="_blank">Tap here to Visit</a>
                                         </div>
                                     </div>
                                 </div>
@@ -89,7 +104,7 @@ const Projects = () => {
                                         <div className="card-body">
                                             <h5 className="card-title" style={{ textAlign: "center" }}>To-Do List</h5>
                                             <p className="card-text">• Designed Complete Frontend To-Do Website using React and Used Github (gh-pages) for hosting,Has One Extra Feature of maintaining your tasks even after refreshing the page or shutting down the PC/laptop.<br></br><br></br>
-                                                • Technologies used: React.JS ,Bootstrap ,Git ,CSS 
+                                                • Technologies used: React.JS ,Bootstrap ,Git ,CSS
                                             </p>
                                             <a href="https://github.com/Rahul27012000/TodoList/tree/master" className="btn-get-started" target="_blank">Can Check it here</a>&nbsp;
                                             <a href="https://rahul27012000.github.io/TodoList/" className="btn-get-started" target="_blank">Tap here to Visit</a>
@@ -102,8 +117,8 @@ const Projects = () => {
                                         <img className="card-img-top" src={pix} alt="Card image cap" />
                                         <div className="card-body">
                                             <h5 className="card-title" style={{ textAlign: "center" }}>ML based pixel art generation</h5>
-                                            <p className="card-text">This is the group project which we will be doing and Mentoring under ISTE.We will be using  Machine Learning to create pixel art from images. We’ll be using Pix2Pix and cycleGAN models which are generally used for img-to-img translation.We will use this images(characters) in our Game which will be created using Unity and C#   
-</p>
+                                            <p className="card-text">This is the group project which we will be doing and Mentoring under ISTE.We will be using  Machine Learning to create pixel art from images. We’ll be using Pix2Pix and cycleGAN models which are generally used for img-to-img translation.We will use this images(characters) in our Game which will be created using Unity and C#
+                                            </p>
                                             <a href="#" className="btn-get-started" >Currently Ongoing</a>
                                         </div>
                                     </div>
@@ -111,23 +126,24 @@ const Projects = () => {
 
                                 <div className="col-md-4 col-10 mx-auto">
                                     <div className="card">
-                                        <img className="card-img-top" src={weather} alt="Card image cap" style={{ height: '195px' }}/>
+                                        <img className="card-img-top" src={weather} alt="Card image cap" style={{ height: '195px' }} />
                                         <div className="card-body">
                                             <h5 className="card-title" style={{ textAlign: "center" }}>Weather Website</h5>
                                             <p className="card-text">• Done this as part of my learning via watching tutorials,taking help of Google and Made Modifications as per my choice.<br></br>
-                                            • As we enter Some City/Place Name, it will give the Current Temperature/Weather of that Place.I have used hbs,NodeJS,ExpressJS and used CSS for designing.</p>
+                                                • As we enter Some City/Place Name, it will give the Current Temperature/Weather of that Place.I have used hbs,NodeJS,ExpressJS and used CSS for designing.</p><br></br>
                                             <a href="https://github.com/Rahul27012000/WeatherWebsite" className="btn-get-started" target="_blank">Can Check it here</a>
                                         </div>
                                     </div>
                                 </div>
-                            
+
                                 <div className="col-md-4 col-10 mx-auto">
                                     <div className="card">
-                                        <img className="card-img-top" src={dance} alt="Card image cap" />
+                                        <img className="card-img-top" src={dance} alt="Card image cap" style={{ height: '195px' }}/>
                                         <div className="card-body">
                                             <h5 className="card-title" style={{ textAlign: "center" }}>Dance Website</h5>
-                                            <p className="card-text">I have done this as part of my learning via watching tutorials,taking help of Google and Made Modifications as per my choice i have used pug,NodeJS,ExpressJS,mongoose and MongoDB for database and used CSS for designing.</p>
-                                            <a href="https://github.com/Rahul27012000/DanceWebsite/tree/master" className="btn-get-started" target="_blank">Can Check it here</a>
+                                            <p className="card-text">•Done this as part of my learning via watching tutorials,taking help of Google and Made Modifications as per my choice<br></br>
+                                            •Technologies used:Pug,NodeJS,ExpressJS,mongoose and MongoDB for database and used CSS for designing.</p><br></br>
+                                            <a href="https://github.com/Rahul27012000/DanceWebsite/tree/master" className="btn-get-started" target="_blank">Can Check it here</a><br></br>
                                         </div>
                                     </div>
                                 </div>
@@ -141,7 +157,7 @@ const Projects = () => {
                                         </div>
                                     </div>
                                 </div>
-                              
+
 
                                 <div className="col-md-4 col-10 mx-auto">
                                     <div className="card">
@@ -178,15 +194,15 @@ const Projects = () => {
                         <div className="col-10 mx-auto">
                             <div className="row gy-4">
 
-                              
+
                                 <div className="col-md-4 col-10 mx-auto">
                                     <div className="card">
                                         <img className="card-img-top" src={wwt} alt="Card image cap" />
                                         <div className="card-body">
-                                        <br></br>
+                                            <br></br>
                                             <h5 className="card-title" style={{ textAlign: "center" }}>Waste Water Treatment</h5><br></br><br></br>
                                             <p className="card-text">I am doing this Project under ISTE.In this project, we explore how water purification is done on an industrial scale. Modelling of Activated Sludge Process which is a widely used process will be done.<br></br><br></br>
-                                             The model will be implemented using MATLAB-Simulink Software to capture the mathematical model describing the process.<br></br></p><br></br>
+                                                The model will be implemented using MATLAB-Simulink Software to capture the mathematical model describing the process.<br></br></p><br></br>
                                             <a href="#" className="btn-get-started">Done</a>
                                         </div>
                                     </div>
@@ -203,8 +219,8 @@ const Projects = () => {
                                     </div>
                                 </div>
 
-                                
-                                
+
+
                             </div>
                         </div>
                     </div>
